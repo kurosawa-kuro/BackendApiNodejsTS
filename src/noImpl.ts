4def572bb5b02f26dc667b81dd9b74d3fb678b9f
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-12-26 08:09:05 +0900

    微調整

--- src/noImpl.ts
+++ src/noImpl.ts
@@ -1,7 +1,6 @@
 import * as Express from 'express';
 
 export default function notImplemented(
-    req: Express.Request, res: Express.Response, next: Express.NextFunction
-    ) {
+    req: Express.Request, res: Express.Response, next: Express.NextFunction) {
     res.status(501).json({ message: 'Not Implemented.' });
 }
