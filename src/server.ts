b02e9a499cfee4dc57bcb9f77f2aef245a17cfaa
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-12-26 08:16:17 +0900

    mongo

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import expressListEndpoints from 'express-list-endpoints';
 import article from './routes/article';
 import auth from './routes/auth';
 import user from './routes/user';
+import dev from './routes/dev';
 
 const app = Express();
 
 app.use('/article', article);
 app.use('/auth', auth);
 app.use('/user', user);
+app.use('/dev', dev);
 
 app.listen(3000, () => {
     console.log('Listen started at port 3000.');
